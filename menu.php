7fee1d03c33d482c14c720fb8d2e290f22b5d7cc
syahirahshamshudin <sharifahsyahirah1351@gmail.com>
2021-10-11 13:01:39 +0800

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,11 @@
+<h1>ARRAY</h1>
+
+[
+    <a href="index.php">numeric array</a>
+    |
+    <a href="assoc.php">associative array</a>
+    |
+    <a href="multi.php">multidimensional array</a>
+    ]
+    <hr>
+    
